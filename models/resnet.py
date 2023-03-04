984527b73098dbaac8a414eb479f307a499e5a05
Omead Pooladzandi <opooladz@ucla.edu>
2023-03-03 17:49:44 -0800

    added shortcut connection flag

--- models/resnet.py
+++ models/resnet.py
@@ -22,7 +22,7 @@ def soft_lrelu(x):
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1,shortcut_connection=True):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False
@@ -59,7 +59,7 @@ class BasicBlock(nn.Module):
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1,shortcut_connection=True):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
@@ -97,23 +97,23 @@ class Bottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, block, num_blocks, num_classes=10,shortcut_connection=True):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1,shortcut_connection=shortcut_connection)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2,shortcut_connection=shortcut_connection)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2,shortcut_connection=shortcut_connection)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2,shortcut_connection=shortcut_connection)
         self.linear = nn.Linear(512 * block.expansion, num_classes)
 
-    def _make_layer(self, block, planes, num_blocks, stride):
+    def _make_layer(self, block, planes, num_blocks, stride,shortcut_connection=True):
         strides = [stride] + [1] * (num_blocks - 1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
+            layers.append(block(self.in_planes, planes, stride,shortcut_connection=shortcut_connection))
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
@@ -129,27 +129,27 @@ class ResNet(nn.Module):
         return out
 
 
-def ResNet18():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
+def ResNet18(shortcut_connection):
+    return ResNet(BasicBlock, [2, 2, 2, 2],shortcut_connection=shortcut_connection)
 
 
-def ResNet34():
-    return ResNet(BasicBlock, [3, 4, 6, 3])
+def ResNet34(shortcut_connection):
+    return ResNet(BasicBlock, [3, 4, 6, 3],shortcut_connection=shortcut_connection)
 
 
-def ResNet50():
-    return ResNet(Bottleneck, [3, 4, 6, 3])
+def ResNet50(shortcut_connection):
+    return ResNet(Bottleneck, [3, 4, 6, 3],shortcut_connection=shortcut_connection)
 
 
-def ResNet101():
-    return ResNet(Bottleneck, [3, 4, 23, 3])
+def ResNet101(shortcut_connection):
+    return ResNet(Bottleneck, [3, 4, 23, 3],shortcut_connection=shortcut_connection)
 
 
-def ResNet152():
-    return ResNet(Bottleneck, [3, 8, 36, 3])
+def ResNet152(shortcut_connection):
+    return ResNet(Bottleneck, [3, 8, 36, 3],shortcut_connection=shortcut_connection)
 
 def test():
-    net = ResNet18()
+    net = ResNet18(shortcut_connection=True)
     y = net(torch.randn(1, 3, 32, 32))
     print(y.size())
 
